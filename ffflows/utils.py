c1a86b7edc09f354bcdb17430c2d2584118e581e
Johnny Raine <johnny.raine@cern.ch>
2022-10-03 17:33:40 +0200

    Update utils.py for lower fix

--- ffflows/utils.py
+++ ffflows/utils.py
@@ -9,6 +9,6 @@ def getActivation(activation, *args, **kwargs):
         "selu" : nn.SELU(*args, **kwargs),
         "softplus" : nn.Softplus(*args, **kwargs),
     }
-    assert lower(activation) in actdict, f"Currently {activation} is not supported"
+    assert activation.lower() in actdict, f"Currently {activation} is not supported"
     
-    return actdict(activation, *args, **kwargs)
+    return actdict(activation.lower(), *args, **kwargs)
