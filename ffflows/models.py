4261108dfa105e537a467c7a3e253008e2b48a7f
Johnny Raine <johnny.raine@cern.ch>
2022-10-07 15:11:13 +0200

    Semantic reording

--- ffflows/models.py
+++ ffflows/models.py
@@ -1,5 +1,7 @@
-from nflows import transforms
+from re import X
+from nflows import transforms, flows
 from torch.nn import functional as F
+import torch.nn as nn
 
 from .utils import getActivation
 
@@ -36,33 +38,24 @@ class DenseNet(nn.Module):
             x = layer(x)
         return x
 
-class FlowForFlowModel(nn.module):
+class FlowForFlow(flows.Flow):
     '''Driving class for a flow for flow model.
     Holds the top flow as well as base distributions, and handles training steps for forward and backward.
     '''
-    def __init__(self):
-        super(self,FlowForFlowModel).__init__()
-        self.transform = None
-        self.base_flow_fwd = None
-        self.base_flow_inv = None
-        self.configured = 0b000
+    def __init__(self, transform, distribution_fwd, distribution_inv=None, embedding_net=None, context_func=None):
+        self.base_flow_fwd = distribution_fwd
+        self.base_flow_inv = distribution_inv if distribution_inv is not None else distribution_fwd
+        self.context_func = context_func if context_func is not None else nn.Identity
+        super(self,FlowForFlow).__init__(transform, distribution_fwd, embedding_net)
+        
+    def set_forward_base(self):
+        '''Just in case we need to change the base distribution in the subclass'''
+        self._distibution = self.base_flow_fwd
+        
+    def set_backward_base(self):
+        '''Just in case we need to change the base distribution in the subclass'''
+        self._distibution = self.base_flow_inv
 
-    def setTransform(self, transform):
-        '''Set the transformer for the flows4flows model.
-        This should be an nflows transformer'''
-        self.transform = transform
-        self.configured |= 1
-
-    def setBaseFlows(self, base_fwd, base_inv=None):
-        '''Set the base distribution flows for the forward (and optionally inverse if different) passes.'''
-        self.base_flow_fwd = base_fwd
-        self.configured |= 1<<1
-        if base_inv is not None:
-            self.base_flow_inv = base_inv
-        else:
-            self.base_flow_inv = base_fwd
-        self.configured |= 1<<2
-    
     def transform_and_log_prob(self, x, context=None, inverse=False):
         '''Transform inputs through top transformer. Inverse pass possible.
         Optionally pass a context tuple. Each element of the tuple will be passed as the context to the respective base distribution.'''
@@ -79,35 +72,51 @@ class FlowForFlowModel(nn.module):
             logprob = self.base_flow_fwd._log_prob(y, context_r)
         return y, logprob - logabsdet
     
-    def forward(self, x, context=None, inverse=False):
-        assert self.configured & 0b001, "Must have top flow configured"
-        # context_l,context_r = context
+    def transform(self,inputs, context=None, inverse=False):
+        '''Transform inputs with transformer given context. Choose dorward (defualt) or inverse (set to true) pass'''
         if inverse:
-            y, logabsdet = self.topflow._transform.inverse(x, context=context)
+            y, logabsdet = self._transform.inverse(inputs, context=context)
         else:
-            y, logabsdet = self.topflow._transform(x, context=context)
+            y, logabsdet = self._transform(inputs, context=context)
         
         return y, logabsdet
 
-    def _log_prob(self, x, context=None, inverse=False):
-        _, logprob = self.transform_and_log_prob(x, context=context, inverse=inverse)
-        return logprob
+    def _log_prob(self, inputs, context=None, inverse=False):
+        '''log probability of transformed inputs given context, use relevant base distribution based on forward or inverse.'''
+        context_l,context_r = context
+        embedded_context = self._embedding_net(self.context_func(context))
+        
+        noise, logabsdet = self.transform(inputs, context=embedded_context, inverse=inverse)
+        if self._context_used_in_base:
+            con = context_l if inverse else context_r
+            log_prob = self._distribution.log_prob(noise, context=con)
+        else:
+            log_prob = self._distribution.log_prob(noise)
+        return log_prob + logabsdet
 
 
-    def sample(self, num_samples, context, inverse=False):
+    def _sample(self, num_samples, context):#, inverse=False):
         '''Something we will never likely need. Sample from a base distribution and pass through the top transformer.'''
-        assert self.configured & 0b111, "Must have top flow and base flows configured"
-        if context is None:
-            context_l,context_r = (None, None)
-        else:
-            context_l,context_r = context
-        if inverse:
-            x = self.base_flow_fwd._sample(num_samples, context = context_r)
-            samples = self.transform.inverse(x, context = context)
-        else:
-            x = self.base_flow_inv._sample(num_samples, context = context_l)
-            samples = self.transform(x, context = context)
-        return samples
+        raise NotImplementedError()# "Shouldn't really want to sample from a base distribution with flows for flows"
+        # if context is None:
+        #     context_l,context_r = (None, None)
+        # else:
+        #     embedded_context = self._embedding_net(context)
+        #     context_l,context_r = context
+        # if inverse:
+        #     x = self.base_flow_fwd._sample(num_samples, context = context_r)
+        #     samples,_ = self._passthrough(x, context=embedded_context, inverse=inverse)
+        # else:
+        #     x = self.base_flow_inv._sample(num_samples, context = context_l)
+        #     samples,_ = self._passthrough(x, context=embedded_context, inverse=inverse)
+        # return samples
 
-    def forward(self, x, context, inverse=False):
-        y, log_prob = self._log_prob(x, context=context,inverse=inverse)
+    # def forward(self, x, context, inverse=False):
+    #     y, log_prob = self._log_prob(x, context=context,inverse=inverse)
+
+    def sample_and_log_prob(self, num_samples, context=None):
+        raise NotImplementedError()
+
+    # def compute_loss(self, data):
+    #         data, context = self.split_data(data)
+    #         return -self.log_prob(data, context).mean()
