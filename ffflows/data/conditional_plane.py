8ba802a65b9c346c47f74bb03e23556abafa1b41
Samuel Byrne Klein <samuel.byrne.klein@cern.ch>
2022-10-18 14:53:13 +0200

    Updated wrapper base and rotations.

--- ffflows/data/conditional_plane.py
+++ ffflows/data/conditional_plane.py
@@ -1,3 +1,4 @@
+import torch
 from nflows.utils import tensor2numpy
 
 from ffflows.data.plane import PlaneDataset
@@ -6,20 +7,25 @@ import numpy as np
 
 class ConditionalWrapper(PlaneDataset):
 
+    def __init__(self, base_dataset):
+        super(RotatedData, self).__init__(base_dataset.num_points)
+        self.base_dataset = base_dataset
+
     def _get_data(self):
         return 0, 0
 
-    def _create_data(self):
-        data, condition = self._get_data()
+    def _create_data(self, **kwargs):
+        data, condition = self._get_data(**kwargs)
+        if not torch.is_tensor(condition):
+            data, condition = [torch.Tensor(x).to(self.base_dataset.data) for x in [data, condition]]
         return [[d, r] for d, r in zip(data, condition.view(-1, 1))]
 
 
 class RotatedData(PlaneDataset):
 
     def __init__(self, base_dataset, max_angle=360):
-        super(RotatedData, self).__init__(base_dataset.num_points)
+        super(RotatedData, self).__init__(base_dataset)
         self.max_angle = max_angle
-        self.base_dataset = base_dataset
 
     def make_rot_matrix(self, angle):
         theta = np.radians(angle)
@@ -36,11 +42,12 @@ class RotatedData(PlaneDataset):
             R = np.array([self.make_rot_matrix(theta) for theta in angle])
             return np.einsum('ij,ijk->ik', data, R)
 
-    def _get_data(self):
+    def _get_data(self, angles=None):
         # write angle in degrees
-        angles = np.random.randint(0, self.max_angle, self.n_points)
+        if angles is None:
+            angles = np.random.randint(0, self.max_angle, self.n_points)
         cond_data = self.rotate(self.base_dataset.data, angles)
-        # Assume data lies in [-4, 4] and scale so 45 degree rotation still in this square
+        # Assume data lies in [-4, 4] and scale so if a 45 degree rotation is applied the data is still in this square
         scale = np.sqrt(2 * 4 ** 2)
         data = cond_data / scale * 4
         return data, angles / self.max_angle
