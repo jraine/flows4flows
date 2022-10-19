78c1db2bb8dd8dd73e4f11cdeac5c8b9dde71bfd
Johnny Raine <johnny.raine@cern.ch>
2022-10-19 17:56:44 +0200

    Fixing typo in jointbase training Was using base dist nepochs and lr for f4f

--- run_jointbase.py
+++ run_jointbase.py
@@ -95,7 +95,7 @@ def main(cfg: DictConfig) -> None:
     else:
         print("Training Flow4Flow model")
         train_f4f(f4flow, base_data, val_base_data,
-                  cfg.base_dist.nepochs, cfg.base_dist.lr, cfg.general.ncond,
+                  cfg.top_transformer.nepochs, cfg.top_transformer.lr, cfg.general.ncond,
                   outputpath, name='f4f', device=device)
 
     f4flow.to(device)
