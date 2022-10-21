8d1ca856b8ef7b706d3d30226c7020ea6cbfb4c8
Samuel Byrne Klein <samuel.byrne.klein@cern.ch>
2022-10-21 11:48:50 +0200

    Added Johnnys custom penalty.

--- ffflows/distance_penalties.py
+++ ffflows/distance_penalties.py
@@ -12,6 +12,15 @@ class BasePenalty(nn.Module):
         return self.weight * 0
 
 
+class CustomPenalty(BasePenalty):
+    def __init__(self, lambda_func, weight=1.0):
+        self.lambda_fn = lambda_func
+        super(CustomPenalty, self).__init__(weight)
+
+    def forward(self, inputs, outputs):
+        return self.weight * self.lambda_fn(inputs, outputs)
+
+
 class LOnePenalty(BasePenalty):
 
     def forward(self, inputs, outputs):
