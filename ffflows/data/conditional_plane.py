Hiding broken pandas stuff and also fixing the radial scaling datasets to have a minimum scaling value so things to map to zero!!

ffflows/data/conditional_plane.py
@@ -55,7 +55,7 @@ class ConditionalWrapper(ConditionalPlaneDataset):
 
 class RotatedData(ConditionalWrapper):
 
-    def __init__(self, base_dataset, max_angle=360):
+    def __init__(self, base_dataset, max_angle=45):
         self.max_angle = max_angle
         super(RotatedData, self).__init__(base_dataset)
 
@@ -91,8 +91,9 @@ class RotatedData(ConditionalWrapper):
 
 class RadialScale(ConditionalWrapper):
 
-    def __init__(self, base_dataset, max_scale=1):
+    def __init__(self, base_dataset, max_scale=1, minscale=0.2):
         self.max_scale = max_scale
+        self.min_scale = minscale
         super(RadialScale, self).__init__(base_dataset)
 
     def _get_conditional(self, scale=None):
@@ -100,14 +101,15 @@ class RadialScale(ConditionalWrapper):
         if scale is None:
             # When sampling in 2D there is a volume correction for the radius which is accounted for with the square
             # root
-            scale = np.random.rand(self.num_points).reshape(-1, 1) ** 0.5 * self.max_scale
+            scale = np.random.rand(self.num_points).reshape(-1, 1) ** 0.5 * (
+                        self.max_scale - self.min_scale) + self.min_scale
         cond_data = self.base_dataset.data * scale
         return cond_data, scale
 
     def get_default_eval(self, n_test):
         """Set the data to some default condition and return a set of default points."""
         self._create_data(scale=1)
-        return torch.linspace(0, self.max_scale, n_test + 1)[1:]
+        return torch.linspace(self.min_scale, self.max_scale, n_test + 1)[1:]
 
 
 class ElipseShift(ConditionalWrapper):

plot.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import pandas as pd
+# import pandas as pd
 from nflows.utils import tensor2numpy
 import numpy as np
 from scipy.stats import binned_statistic_2d
@@ -76,9 +76,9 @@ def plot_arrays(dict_of_data, sv_dir, sv_nm, colors=None):
         set_bounds(ax[i])
     fig.savefig(sv_dir / f'colored_{sv_nm}.png')
 
-    # TODO want to do something like this but pandas is broken in the container?
-    data = {k: tensor2numpy(x) for k, x in dict_of_data.items()}
-    ln, r = data[list(data.keys())[0]].shape
-    df = pd.DataFrame({k: x.ravel() for k, x in data.items()},
-                      index=pd.MultiIndex.from_product([np.arange(ln), np.arange(r)]))
-    df.to_csv(sv_dir / f'{sv_nm}.csv', index=False)
+    # # TODO want to do something like this but pandas is broken in the container?
+    # data = {k: tensor2numpy(x) for k, x in dict_of_data.items()}
+    # ln, r = data[list(data.keys())[0]].shape
+    # df = pd.DataFrame({k: x.ravel() for k, x in data.items()},
+    #                   index=pd.MultiIndex.from_product([np.arange(ln), np.arange(r)]))
+    # df.to_csv(sv_dir / f'{sv_nm}.csv', index=False)